f1b02866c50f4a56a402af4cb5799daaf7200d14
OgGhostJelly <johnnycambodia09@gmail.com>
2025-10-03 00:19:27 +0700

    Waaay faster

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::{io::Read, num::ParseIntError};
+use std::{
+    io::{self, BufReader, Read},
+    mem,
+    num::ParseIntError,
+};
 
 use ureq::{
     Agent,
@@ -11,7 +15,11 @@ pub fn extract_file(agent: &Agent, uri: Uri, filesize: Option<usize>, name: &[u8
         None => request_content_length(agent, &uri)?,
     };
 
-    find_in_central_directory(agent, &uri, filesize, name)?;
+    let start = std::time::Instant::now();
+    let Some(cfdh) = find_in_central_directory(agent, &uri, filesize, name)? else {
+        return Err(Error::FileNotFound);
+    };
+    println!("Finished in {:?}", std::time::Instant::now() - start);
 
     todo!()
 }
@@ -20,12 +28,18 @@ pub fn extract_file(agent: &Agent, uri: Uri, filesize: Option<usize>, name: &[u8
 ///
 /// # Errors
 /// If HTTP range requests are not supported.
-fn find_in_central_directory(agent: &Agent, uri: &Uri, filesize: usize, name: &[u8]) -> Result<()> {
+fn find_in_central_directory(
+    agent: &Agent,
+    uri: &Uri,
+    filesize: usize,
+    name: &[u8],
+) -> Result<Option<CDFH>> {
     const CHUNK_SIZE: usize = 1_048_576; // 1 MB
 
     let chunks = filesize.div_ceil(CHUNK_SIZE);
 
     let mut bytes_from_last_read = vec![];
+    let mut maximum_allowed_offset = filesize;
 
     for chunk_idx in 0..chunks {
         let from = filesize
@@ -40,30 +54,51 @@ fn find_in_central_directory(agent: &Agent, uri: &Uri, filesize: usize, name: &[
             .header("Range", format!("bytes={from}-{to}"))
             .call()?;
 
-        let mut reader = req.into_body().into_reader();
-        let mut buf = Vec::with_capacity(to - from);
-        let start = std::time::Instant::now();
-        reader.read_to_end(&mut buf).unwrap();
-        buf.append(&mut bytes_from_last_read);
-        println!("Read to buffer in {:?}", std::time::Instant::now() - start);
+        let reader = BufReader::new(req.into_body().into_reader());
+        let append = mem::take(&mut bytes_from_last_read).into_iter().map(Ok);
+        let mut reader = reader.bytes().chain(append);
+
+        let mut buf = match RingBuffer::try_from_iter(&mut reader)? {
+            Ok(buf) => buf,
+            Err(mut buf) => {
+                bytes_from_last_read.append(&mut buf);
+                continue;
+            }
+        };
 
         let mut is_last_chunk = false;
         let mut stray_bytes = true;
+        let mut byte_offset = from;
 
-        for i in 0..buf.len() {
-            if buf[i..].starts_with(b"PK\x03\x04") {
+        while let Some(value) = reader.next() {
+            let value = value?;
+            buf.push(value);
+
+            if buf.buf == *b"PK\x03\x04" {
+                // Found a file header.
+                // which means this data must be outside of the central directory record,
+                // and this is the last chunk we need to process.
                 stray_bytes = false;
                 is_last_chunk = true;
-            } else if buf[i..].starts_with(b"PK\x01\x02") {
+            } else if buf.buf == *b"PK\x01\x02" {
+                // Found a central directory file header.
                 stray_bytes = false;
-                println!("Found CDFH at chunk relative offset {i}");
-                let filename_len =
-                    u16::from_le_bytes(buf[i + 28..i + 30].try_into().unwrap()) as usize;
-                let filename = str::from_utf8(&buf[i + 46..i + 46 + filename_len]).unwrap();
-                println!("{filename}")
+
+                if let Some(cdfh) = read_cdfh(&mut reader, maximum_allowed_offset)? {
+                    if cdfh.filename == name {
+                        return Ok(Some(cdfh));
+                    }
+                }
+            } else if buf.buf == *b"PK\x05\x06" {
+                // Reached the end of the central directory record (EOCD).
+                // no offset should be after the EOCD, so set the maximum allowed offset.
+                maximum_allowed_offset = byte_offset;
+                break;
             } else if stray_bytes {
-                bytes_from_last_read.push(buf[i]);
+                bytes_from_last_read.push(value);
             }
+
+            byte_offset += 1;
         }
 
         if is_last_chunk {
@@ -71,12 +106,180 @@ fn find_in_central_directory(agent: &Agent, uri: &Uri, filesize: usize, name: &[
         }
     }
 
-    todo!()
+    Ok(None)
+}
+
+/// Read a central directory file header or None if it is a false positive.
+/// The iterator should return bytes right after the magic number.
+fn read_cdfh<I: Iterator<Item = io::Result<u8>>>(
+    reader: &mut I,
+    maximum_allowed_offset: usize,
+) -> Result<Option<CDFH>> {
+    let version_made_by = read_u16(reader)?;
+    let minimum_required_version = read_u16(reader)?;
+    // The version is stored in the last 8 bits of the field,
+    // if the version is larger than 63 it's likely a false positive.
+    if (version_made_by & 0xff) > 63 || (minimum_required_version & 0xff) > 63 {
+        return Ok(None);
+    }
+    let general_purpose_flags = read_u16(reader)?;
+    let Some(compression_method) = CompressionMethod::from_id(read_u16(reader)?) else {
+        return Ok(None);
+    };
+    let last_modification_time = read_u16(reader)?;
+    let last_modification_date = read_u16(reader)?;
+    let crc32 = read_u32(reader)?;
+    let compressed_size = read_u32(reader)?;
+    let uncompressed_size = read_u32(reader)?;
+    let filename_length = read_u16(reader)?;
+    let extra_field_length = read_u16(reader)?;
+    let file_comment_length = read_u16(reader)?;
+    let disk_number = read_u16(reader)?;
+    let internal_attrs = read_u16(reader)?;
+    let external_attrs = read_u32(reader)?;
+    let file_header_offset = read_u32(reader)?;
+    if file_header_offset as usize > maximum_allowed_offset {
+        return Ok(None);
+    }
+
+    let filename = read_bytes(reader, filename_length as usize)?;
+    let _extra_field = skip_bytes(reader, extra_field_length as usize)?;
+    let _file_comment = skip_bytes(reader, file_comment_length as usize)?;
+
+    Ok(Some(CDFH {
+        version_made_by,
+        minimum_required_version,
+        general_purpose_flags,
+        compression_method,
+        last_modification_time,
+        last_modification_date,
+        crc32,
+        compressed_size,
+        uncompressed_size,
+        filename_length,
+        extra_field_length,
+        file_comment_length,
+        disk_number,
+        internal_attrs,
+        external_attrs,
+        file_header_offset,
+        filename,
+    }))
+}
+
+struct CDFH {
+    version_made_by: u16,
+    minimum_required_version: u16,
+    general_purpose_flags: u16,
+    compression_method: CompressionMethod,
+    last_modification_time: u16,
+    last_modification_date: u16,
+    crc32: u32,
+    compressed_size: u32,
+    uncompressed_size: u32,
+    filename_length: u16,
+    extra_field_length: u16,
+    file_comment_length: u16,
+    disk_number: u16,
+    internal_attrs: u16,
+    external_attrs: u32,
+    file_header_offset: u32,
+    filename: Vec<u8>,
+}
+
+#[derive(Debug)]
+enum CompressionMethod {
+    Stored,
+    Deflated,
+    Deflate64,
+    Bzip2,
+    LZMA,
+    Zstd,
+    XZ,
+    AES,
+}
+
+impl CompressionMethod {
+    pub fn from_id(value: u16) -> Option<CompressionMethod> {
+        Some(match value {
+            0 => Self::Stored,
+            8 => Self::Deflated,
+            9 => Self::Deflate64,
+            12 => Self::Bzip2,
+            14 => Self::LZMA,
+            93 => Self::Zstd,
+            95 => Self::XZ,
+            99 => Self::AES,
+            _ => return None,
+        })
+    }
+}
+
+fn skip_bytes<I: Iterator<Item = io::Result<u8>>>(iter: &mut I, len: usize) -> Result<()> {
+    for _ in 0..len {
+        _ = opt2eof(iter.next())?;
+    }
+    Ok(())
+}
+
+fn read_bytes<I: Iterator<Item = io::Result<u8>>>(iter: &mut I, len: usize) -> Result<Vec<u8>> {
+    let mut buf = Vec::with_capacity(len);
+    for _ in 0..len {
+        buf.push(opt2eof(iter.next())?);
+    }
+    Ok(buf)
+}
+
+fn read_u32<I: Iterator<Item = io::Result<u8>>>(iter: &mut I) -> Result<u32> {
+    Ok(u32::from_le_bytes([
+        opt2eof(iter.next())?,
+        opt2eof(iter.next())?,
+        opt2eof(iter.next())?,
+        opt2eof(iter.next())?,
+    ]))
+}
+
+fn read_u16<I: Iterator<Item = io::Result<u8>>>(iter: &mut I) -> Result<u16> {
+    Ok(u16::from_le_bytes([
+        opt2eof(iter.next())?,
+        opt2eof(iter.next())?,
+    ]))
+}
+
+#[inline]
+fn opt2eof<T>(value: Option<io::Result<T>>) -> Result<T> {
+    match value {
+        Some(value) => Ok(value?),
+        None => Err(Error::UnexpectedEof),
+    }
 }
 
 struct RingBuffer<T, const N: usize> {
     buf: [T; N],
     ptr: usize,
+}
+
+impl<T, const N: usize> RingBuffer<T, N> {
+    pub fn try_from_iter<I: Iterator<Item = io::Result<T>>>(
+        iter: I,
+    ) -> io::Result<std::result::Result<Self, Vec<T>>> {
+        let mut vals: Vec<T> = Vec::with_capacity(N);
+
+        for value in iter.take(N) {
+            vals.push(value?);
+        }
+
+        match vals.try_into() {
+            Ok(buf) => Ok(Ok(Self::new(buf))),
+            Err(e) => Ok(Err(e)),
+        }
+    }
+}
+
+impl<T, const N: usize> RingBuffer<T, N> {
+    pub fn new(buf: [T; N]) -> Self {
+        Self { buf, ptr: 0 }
+    }
 }
 
 impl<T, const N: usize> RingBuffer<T, N> {
@@ -123,4 +326,10 @@ pub enum Error {
     MalformedContentLengthToStr(ToStrError),
     #[error("malformed content-length: {0}")]
     MalformedContentLengthParseInt(ParseIntError),
+    #[error("extract file: {0}")]
+    Io(#[from] io::Error),
+    #[error("unexpected eof")]
+    UnexpectedEof,
+    #[error("file not found")]
+    FileNotFound,
 }
